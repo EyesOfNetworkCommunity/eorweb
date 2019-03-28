c34c09bf96b909e45db41c8a1c70e7ac63d802f4
maxtkl <maxime287@hotmail.com>
2019-03-28 12:13:00 -0400

    fix message place when the request is update

--- module/birt_remediation/index.php
+++ module/birt_remediation/index.php
@@ -42,47 +42,49 @@ $remediation_action_selected = retrieve_form_data("remediation_action_selected",
 $remediation_selected = retrieve_form_data("remediation_selected",null);
 $validator_right = sqlrequest($database_eorweb,"SELECT validator FROM groups WHERE group_id = ?",false,array("i",(int)$group_id));
 $remediation_right = mysqli_result($validator_right,0,"validator");
-
-switch($actions){
-	case "validation":
-		if(isset($remediation_selected[0])) {
-			for ($i = 0; $i < sizeof($remediation_selected); $i++) {
-				// Update remediations state
-				sqlrequest($database_eorweb,"UPDATE remediation SET state='approved', date_validation='".date("Y-m-d G:i")."' WHERE id='$remediation_selected[$i]'");
-			}
-			message(6," : ".getLabel("message.manage_remediation.request_update"),'ok');
-		}
-		break;
-	case "refus":
-		if(isset($remediation_selected[0])) {
-			$message_update = false;
-			for ($i = 0; $i < sizeof($remediation_selected); $i++) {
-				$req = sqlrequest($database_eorweb, "SELECT state FROM remediation WHERE id='$remediation_selected[$i]'");
-				$execute_request = mysqli_result($req,0,"state");
-				// Update remediations state if request-remediation has not been executed
-				if ($execute_request != "executed") {
-					sqlrequest($database_eorweb,"UPDATE remediation SET state='refused', date_validation='".date("Y-m-d G:i")."' WHERE id='$remediation_selected[$i]'");
-					$message_update = true;
-				}
-			}
-			// display message if a request_remediation has been updated
-			if ($message_update) {
-				message(6," : ".getLabel("message.manage_remediation.request_update"),'ok');
-			} else {
-				message(6," : ".getLabel("message.manage_remediation.request_not_update"),'warning');
-			}
-		}
-		break;
-}?> 
+?> 
 
 <div id="page-wrapper">
-	
+
 	<div class="row">
 		<div class="col-lg-12">
 			<h1 class="page-header"><?php echo getLabel("label.manage_remediation.list_remediations"); ?></h1>
 		</div>
 	</div>
 
+	<?php
+		switch($actions){
+		case "validation":
+			if(isset($remediation_selected[0])) {
+				for ($i = 0; $i < sizeof($remediation_selected); $i++) {
+					// Update remediations state
+					sqlrequest($database_eorweb,"UPDATE remediation SET state='approved', date_validation='".date("Y-m-d G:i")."' WHERE id='$remediation_selected[$i]'");
+				}
+				message(6," : ".getLabel("message.manage_remediation.request_update"),'ok');
+			}
+			break;
+		case "refus":
+			if(isset($remediation_selected[0])) {
+				$message_update = false;
+				for ($i = 0; $i < sizeof($remediation_selected); $i++) {
+					$req = sqlrequest($database_eorweb, "SELECT state FROM remediation WHERE id='$remediation_selected[$i]'");
+					$execute_request = mysqli_result($req,0,"state");
+					// Update remediations state if request-remediation has not been executed
+					if ($execute_request != "executed") {
+						sqlrequest($database_eorweb,"UPDATE remediation SET state='refused', date_validation='".date("Y-m-d G:i")."' WHERE id='$remediation_selected[$i]'");
+						$message_update = true;
+					}
+				}
+				// display message if a request_remediation has been updated
+				if ($message_update) {
+					message(6," : ".getLabel("message.manage_remediation.request_update"),'ok');
+				} else {
+					message(6," : ".getLabel("message.manage_remediation.request_not_update"),'warning');
+				}
+			}
+			break;
+	} ?>
+	
 	<form action="./index.php" method="POST">
 		<div class="dataTable_wrapper">
 			<table class="table table-striped datatable-eorweb table-condensed">
